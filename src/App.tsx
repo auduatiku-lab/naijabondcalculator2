96274bff831e88ed6e3a886081fc1a9c40b03de0
auduatiku-lab <auduatiku@gmail.com>
2026-04-20 12:32:38 +0100

    feat: Remove outdated bond data
    Removes the "10.00% FGN BOND AUG 2033" entry from the list of available bonds. This bond is no longer current and should not be presented to users.

--- src/App.tsx
+++ src/App.tsx
@@ -140,7 +140,6 @@ export default function App() {
         { name: "15.64% FGN SUKUK BOND DEC 2032", couponRate: 15.64, maturityDate: "02-DEC-2032" },
         { name: "19.8900% FGN BOND MAY 2033", couponRate: 19.89, maturityDate: "15-MAY-2033" },
         { name: "14.7000% FGN BOND JUN 2033", couponRate: 14.70, maturityDate: "21-JUN-2033" },
-        { name: "10.00% FGN BOND AUG 2033", couponRate: 10.00, maturityDate: "16-AUG-2033" },
         { name: "15.7500% FGN SUKUK BOND OCT 2033", couponRate: 15.75, maturityDate: "13-OCT-2033" },
         { name: "19.0000% FGN BOND FEB 2034", couponRate: 19.00, maturityDate: "21-FEB-2034" },
         { name: "12.1493% FGN BOND JUL 2034", couponRate: 12.1493, maturityDate: "18-JUL-2034" },
